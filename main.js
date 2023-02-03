1c60d0b4057a93f48227579ff85c176f025155ce
Alex <aleksander.misterkiewicz@outlook.com>
2023-02-03 18:21:07 +0100

    init commit

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
+// main.js
+
+// Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
-    const win = new BrowserWindow({
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
-    win.loadFile('index.html')
+    mainWindow.loadFile('index.html')
+    mainWindow.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
     createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
 })
